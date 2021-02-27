18c765c691f77d61a23e8d3f3cebe2fa48e29302
ballicipluck <shyam4564@gmail.com>
2021-02-27 11:24:02 +0530

    add thumbnail

--- bot.py
+++ bot.py
@@ -1,6 +1,7 @@
 from pyrogram import Client, filters
 import youtube_dl
 from youtube_search import YoutubeSearch
+import requests
 
 import os
 from config import Config
@@ -34,16 +35,35 @@ def a(client, message):
     m = message.reply('🔎 Fetching the song...')
     ydl_opts = {"format": "bestaudio[ext=m4a]"}
     try:
-        results = YoutubeSearch(query, max_results=1).to_dict()
-        link = f"https://youtube.com{results[0]['url_suffix']}"
-        print(results)
-        title = results[0]["title"]
-        thumbnail = results[0]["thumbnails"][0]
-        duration = results[0]["duration"]
-        views = results[0]["views"]
-        # if time_to_seconds(duration) >= 1800:  # duration limit
-        #     m.edit("Exceeded 30mins cap")
-        #     return
+        results = []
+        count = 0
+        while len(results) == 0 and count < 6:
+            if count>0:
+                time.sleep(1)
+            results = YoutubeSearch(query, max_results=1).to_dict()
+            count += 1
+        # results = YoutubeSearch(query, max_results=1).to_dict()
+        try:
+            link = f"https://youtube.com{results[0]['url_suffix']}"
+            # print(results)
+            title = results[0]["title"]
+            thumbnail = results[0]["thumbnails"][0]
+            duration = results[0]["duration"]
+
+            ## UNCOMMENT THIS IF YOU WANT A LIMIT ON DURATION. CHANGE 1800 TO YOUR OWN PREFFERED DURATION AND EDIT THE MESSAGE (30 minutes cap) LIMIT IN SECONDS
+            # if time_to_seconds(duration) >= 1800:  # duration limit
+            #     m.edit("Exceeded 30mins cap")
+            #     return
+
+            views = results[0]["views"]
+            thumb_name = f'thumb{message.message_id}.jpg'
+            thumb = requests.get(thumbnail, allow_redirects=True)
+            open(thumb_name, 'wb').write(thumb.content)
+
+        except Exception as e:
+            print(e)
+            m.edit('Found nothing. Try changing the spelling a little.')
+            return
     except Exception as e:
         m.edit(
             "✖️ Found Nothing. Sorry.\n\nTry another keywork or maybe spell it properly."
@@ -56,25 +76,19 @@ def a(client, message):
             info_dict = ydl.extract_info(link, download=False)
             audio_file = ydl.prepare_filename(info_dict)
             ydl.process_info(info_dict)
-        rep = f'🎧 Title: `{title}`\n⏳ Duration: `{duration}`\n👁‍🗨 Views: `{views}`'
+        rep = f'🎧 **Title**: [{title}]({link})\n⏳ **Duration**: `{duration}`\n👁‍🗨 **Views**: `{views}`'
         secmul, dur, dur_arr = 1, 0, duration.split(':')
         for i in range(len(dur_arr)-1, -1, -1):
             dur += (int(dur_arr[i]) * secmul)
             secmul *= 60
-        message.reply_audio(audio_file, caption=rep, parse_mode='md', title=title, duration=dur)
+        message.reply_audio(audio_file, caption=rep, parse_mode='md',quote=False, title=title, duration=dur, thumb=thumb_name)
         m.delete()
     except Exception as e:
         m.edit('❌ Error')
         print(e)
-        # os.rename(audio_file, "audio.webm")
-    # command = 'ffmpeg -i "'+ audio_file + '" -vn "' + audio_file[:-4] + '.m4a"'
-    # m.edit("Converting...")
-    # p = subprocess.Popen(command, stdout=subprocess.PIPE, shell=False)
-    # p.wait()
-
     try:
         os.remove(audio_file)
-        # os.remove(audio_file[:-4] + '.m4a')
+        os.remove(thumb_name)
     except Exception as e:
         print(e)
 
