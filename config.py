cc5b68555465a09e334dc55935d908020e620f75
sathanxavier1998 <71935696+sathanxavier1998@users.noreply.github.com>
2021-03-14 20:27:28 +0530

    Update and rename sample.config.py to config.py

---
+++ config.py
@@ -0,0 +1,4 @@
+class Config:
+    BOT_TOKEN = 867759608:AAGdgnE5-6DFxVIAh7GoSnxc1dAJ4Hl1i_8
+    API_ID = 1436542
+    API_HASH = ace9d0fb8f46300776a985ca357bdfff
--- sample.config.py
+++
@@ -1,5 +0,0 @@
-class Config:
-    BOT_TOKEN = "" # get it from @botfather (telegram) by sending /newbot command.
-    API_ID =  # Same as APP_ID. Get it from my.telegram.org.
-    API_HASH = "" # Get it from my.telegram.org.
-    
