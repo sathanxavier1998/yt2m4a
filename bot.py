683fdf0a930748ebcef9a6762123e8a82441a2f1
rahulps1000 <58558341+rahulps1000@users.noreply.github.com>
2021-03-21 17:20:43 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -25,7 +25,7 @@ def time_to_seconds(time):
 @bot.on_message(filters.command(['start']))
 def start(client, message):
     mention = f"Hello [{message.from_user.first_name}](tg://user?id={message.from_user.id})  my name is Song Assistant © \n\ni can download any video songs from youtube.  But now i only works for my owner's group.\n\n👉🏻 tap /help to know how to use me ❣️\n\nif you wanna add me to your group plz contact my owner 👇🏻"
-    message.send_message(chat_id=meeage.chat.id, text=mention, reply_to_message_id=message.id,     reply_markup=InlineKeyboardMarkup(
+    bot.send_message(chat_id=meeage.chat.id, text=mention, reply_to_message_id=message.id,     reply_markup=InlineKeyboardMarkup(
         [
             [InlineKeyboardButton("Contact Owner", url="https://t.me/sathan_of_telegram")]
         ]))
