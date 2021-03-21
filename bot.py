cc233a6feea8a25c3f58ead3b49000d0fc837b79
rahulps1000 <58558341+rahulps1000@users.noreply.github.com>
2021-03-21 19:02:33 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -36,10 +36,10 @@ def help(client, message):
     text_msg="**How to Use me..?**\n\n 🔰 /song - **any song name**\n\n 🔰 /song - **any youtube link**\n\n 🔰 /mp3 - **to download songs in mp3 format**\n\n**⚠️ Note: I can only download songs under 10 minutes.\n\nIf you like this bot plz support out Telegram Channels 🦋⃟ 𝄟࿐**"
     bot.send_message(chat_id=message.chat.id, text=text_msg, reply_to_message_id=message.message_id,reply_markup=InlineKeyboardMarkup(
         [
-            [[InlineKeyboardButton("[CT™] Channel", url="https://t.me/cinemaathattakam_chanel")],
-            [InlineKeyboardButton("[CT™] Series", url="https://t.me/cinemaathattakam_series")]
-            [InlineKeyboardButton("പാട്ടു പെട്ടി....🎶🎤", url="https://t.me/paattuppetti")],]
-            [[InlineKeyboardButton("🦋MY DEV🦋", url="https://t.me/sathan_of_telegram")],]
+            [InlineKeyboardButton("[CT™] Channel", url="https://t.me/cinemaathattakam_chanel"),
+            InlineKeyboardButton("[CT™] Series", url="https://t.me/cinemaathattakam_series"),
+            InlineKeyboardButton("പാട്ടു പെട്ടി....🎶🎤", url="https://t.me/paattuppetti")],
+            [InlineKeyboardButton("🦋MY DEV🦋", url="https://t.me/sathan_of_telegram")]
         ]))
     
 @bot.on_message(filters.command(['song']))
