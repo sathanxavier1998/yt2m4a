3df5c589471edfff99cfca42201c43d41e85566b
rahulps1000 <58558341+rahulps1000@users.noreply.github.com>
2021-03-23 20:54:09 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,3 +2,5 @@ class Config:
     BOT_TOKEN = "1633928083:AAG4Z5W1JEdQTkNJRxiiijL6PpBeiQ_fSk8"
     API_ID = 1436542
     API_HASH = "ace9d0fb8f46300776a985ca357bdfff"
+    AUTH_CHANNEL = ""
+    INVITE_MSG = "YOU SHOULD JOIN @......... TO USE THIS BOT"
