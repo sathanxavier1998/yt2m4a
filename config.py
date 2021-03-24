0a3cf96bc1fd03e24cbace38cd5aad885b5e75f7
sathanxavier1998 <71935696+sathanxavier1998@users.noreply.github.com>
2021-03-24 12:38:24 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,4 +3,4 @@ class Config:
     API_ID = 1436542
     API_HASH = "ace9d0fb8f46300776a985ca357bdfff"
     AUTH_CHANNEL = "@cinemaathattakam_chanel"
-    INVITE_MSG = "YOU SHOULD HAVE TO JOIN n/n/[📽️സിനിമ തട്ടകം...🎭🎬™®](https://t.me/cinemaathattakam_chanel) INORDER TO USE ME"
+    INVITE_MSG = "YOU SHOULD HAVE TO JOIN [📽️സിനിമ തട്ടകം...🎭🎬™®](https://t.me/cinemaathattakam_chanel) INORDER TO USE ME"
