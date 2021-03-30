825e595d9059de0fc67039b823ca3200c9986ced
sathanxavier1998 <71935696+sathanxavier1998@users.noreply.github.com>
2021-03-30 10:54:01 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,5 +2,5 @@ class Config:
     BOT_TOKEN = "1633928083:AAG4Z5W1JEdQTkNJRxiiijL6PpBeiQ_fSk8"
     API_ID = 1436542
     API_HASH = "ace9d0fb8f46300776a985ca357bdfff"
-    AUTH_CHANNEL = "@cinemaathattakam_chanel"
+    AUTH_CHANNEL = "@cinemaathattakam_channel"
     INVITE_MSG = "YOU SHOULD HAVE TO JOIN [📽️സിനിമ തട്ടകം...🎭🎬™®](https://t.me/cinemaathattakam_channel) INORDER TO USE ME"
